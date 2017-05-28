f29b3cc641e89c34b4072929a7a3becd35708471
akshatmalik <akshat.ndun@gmail.com>
2017-05-28 15:31:53 +0530

    Transfered Blackboard Reading data to C++

---
+++ Source/TestingGrounds/TP_ThirdPerson/PatrollingGaurd.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGaurd.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API APatrollingGaurd : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
